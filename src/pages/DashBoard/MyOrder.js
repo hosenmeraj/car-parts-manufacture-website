ed444d7f89d86d86c44c983b47f4cbf9961b98e1
Merajhosen <merajakhon7@gmail.com>
2022-05-28 11:27:25 +0600

    verify jwt

--- src/pages/DashBoard/MyOrder.js
+++ src/pages/DashBoard/MyOrder.js
@@ -7,9 +7,22 @@ const MyOrder = () => {
     const [user] = useAuthState(auth)
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/order?email=${user.email}`)
-                .then(res => res.json())
-                .then(data => setOrders(data))
+            fetch(`http://localhost:5000/order?email=${user.email}`, {
+                method: "GET",
+                headers: {
+                    "authorization": `Bearer ${localStorage.getItem("accessToken")}`
+                }
+            })
+                .then(res => {
+                    console.log("res", res)
+                    if (res.status === 401 || res.status === 403) {
+
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setOrders(data)
+                })
         }
     }, [user])
 
