e13101e838388f37f8321b776dd6ab90b26734c5
Merajhosen <merajakhon7@gmail.com>
2022-05-28 13:11:58 +0600

    conditionally admin adding

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const DashBoard = () => {
+    const [user] = useAuthState(auth)
+    const [admin] = useAdmin(user)
     return (
         <div class="drawer drawer-mobile">
             <input id=" dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -19,7 +24,7 @@ const DashBoard = () => {
                     <li><Link to="/dashboard">My Order</Link></li>
                     <li><Link to="/dashboard/reviews">Reviews</Link></li>
                     <li><Link to="/dashboard/profile">My profile</Link></li>
-                    <li><Link to="/dashboard/users">All User</Link></li>
+                    {admin && <li><Link to="/dashboard/users">All User</Link></li>}
 
                 </ul>
 
